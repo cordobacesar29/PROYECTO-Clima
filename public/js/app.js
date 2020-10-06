c70b72a690334bacd2eed811498b5d0d1695691a
Cesar Cordoba <cordobacesar29@gmail.com>
2020-10-06 10:49:26 -0300

    mostramos la información del API en el HTML

--- public/js/app.js
+++ public/js/app.js
@@ -19,6 +19,7 @@ function buscarClima(e) {
     }
 
     //consutamos la API
+    consultarAPI(ciudad, pais);
 }
 
 function mostrarError (mensaje) {
@@ -43,4 +44,95 @@ function mostrarError (mensaje) {
         }, 5000);
     }
 
+}
+
+function consultarAPI(ciudad, pais) {
+
+    const appId = 'cde0696d71e295595848497e80026cd8';
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${ appId }`;
+
+    spinner();
+
+    fetch(url)
+    .then( respuesta => respuesta.json() )
+    .then( datos => {
+        //limpiar el HTML previo
+        limpiarHTL();
+
+        if(datos.cod === '404') {
+            mostrarError('ciudad no encontrada');
+            return;
+        }
+
+        //imprime la respuesta en el HTML
+        mostrarClima( datos );
+    });
+}
+
+function mostrarClima( datos ) {
+    const { name, main: {temp, temp_max, temp_min} } = datos;
+
+    const centigrados = kACentigrados(temp);
+    const max = kACentigrados(temp_max);
+    const min = kACentigrados(temp_min);
+
+    const nombreCiudad = document.createElement('p');
+    nombreCiudad.textContent = `Clima en ${name}`;
+    nombreCiudad.classList.add('font-bold', 'text-2xl');
+
+    const actual = document.createElement('p');
+    actual.innerHTML = `${centigrados} &#8451;`;
+    actual.classList.add('font-bold', 'text-6xl');
+
+    const tempMaxima = document.createElement('p');
+    tempMaxima.innerHTML = `Max: ${max} &#8451;`;
+    tempMaxima.classList.add('text-xl');
+
+    const tempMinima = document.createElement('p');
+    tempMinima.innerHTML = ` Min: ${min} &#8451;`;
+    tempMinima.classList.add('text-xl');
+
+    const resultadoDiv = document.createElement('div');
+    resultadoDiv.classList.add('text-center', 'text-white');
+    resultadoDiv.appendChild(nombreCiudad);
+    resultadoDiv.appendChild(actual);
+    resultadoDiv.appendChild(tempMaxima);
+    resultadoDiv.appendChild(tempMinima);
+    
+    
+    resultado.appendChild(resultadoDiv);
+}
+
+const kACentigrados = grados => parseInt( grados - 273.15 );
+
+function limpiarHTL() {
+    while(resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function spinner() {
+
+    limpiarHTL();
+
+    const divSpinner = document.createElement('div');
+    divSpinner.classList.add('sk-fading-circle');
+
+    divSpinner.innerHTML = `
+    <div class="sk-circle1 sk-circle"></div>
+    <div class="sk-circle2 sk-circle"></div>
+    <div class="sk-circle3 sk-circle"></div>
+    <div class="sk-circle4 sk-circle"></div>
+    <div class="sk-circle5 sk-circle"></div>
+    <div class="sk-circle6 sk-circle"></div>
+    <div class="sk-circle7 sk-circle"></div>
+    <div class="sk-circle8 sk-circle"></div>
+    <div class="sk-circle9 sk-circle"></div>
+    <div class="sk-circle10 sk-circle"></div>
+    <div class="sk-circle11 sk-circle"></div>
+    <div class="sk-circle12 sk-circle"></div>
+    `;
+
+    resultado.appendChild(divSpinner);
 }
